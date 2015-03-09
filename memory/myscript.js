4391570a4ab9e48ff9c98b76ca62a97fc7317c73
abrahamperez <aperez2@kippnyccp.org>
2015-03-09 09:00:46 -0400

    Create myscript.js

---
+++ memory/myscript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  for(i=0; i<20; i++){
+    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
+  }
+  $("div.card"). click(function(){
+    var mynum=0;
+    var firstcard,secondcard;
+    $(this).find('p').css("opacity","1");
+    if(!onfirstclick){
+      //compare cards
+    }else{
+    firstcard=$(this).find('p').html
+  }
+  });
+  });
