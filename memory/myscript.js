90cc0fbdf639f34ec1528f872f5dfd712257015f
abrahamperez <aperez2@kippnyccp.org>
2015-03-11 09:20:42 -0400

    Update myscript.js

--- memory/myscript.js
+++ memory/myscript.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-  for(i=0; i<20; i++){
-    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
+  var isclicked=0;
+  var firstclicked;
+  
+  var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"]
+for(i in card_array){
+    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
   $("div.card"). click(function(){
     var mynum=0;
@@ -11,5 +15,6 @@ $(document).ready(function(){
     }else{
     firstcard=$(this).find('p').html
   }
+    
   });
   });
