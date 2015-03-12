721fd9a7e42246e518daf5f139cb549fcf49185e
abrahamperez <aperez2@kippnyccp.org>
2015-03-12 08:57:02 -0400

    Update javascript.js

--- memory/javascript.js
+++ memory/javascript.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-  var isclicked=0;
-  var firstclicked;
+  var click_count=0;
+  var first_guess=0;
+  var second_guess=0;
+  var first_card=0;
+  
   
   var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"]
 for(i in card_array){
