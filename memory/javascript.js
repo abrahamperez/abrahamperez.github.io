298471e09bc2ddd6c2ef0ef4d2fb3473c94271a9
abrahamperez <aperez2@kippnyccp.org>
2015-03-13 09:08:26 -0400

    Update javascript.js

--- memory/javascript.js
+++ memory/javascript.js
@@ -2,11 +2,18 @@ $(document).ready(function(){
   var click_count=0;
   var first_guess=0;
   var second_guess=0;
-  var first_card=0;
-  
-  
+  var first_card=0;//these are variables that will compare the numbers
   var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"];
-for(i in card_array){
+  function suffle(array){
+    var currentindex=array.length,temporaryvalue,randomindex;
+    //shuffles elements...
+    while(0 !==currentindex){
+      //picks a remainging element
+      randomindex= Math.floor(Math.random() * currentindex):
+      currentindex-=1;
+    }
+  }
+for(i in card_array){//this is the numbers that will appear on each card
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
   
