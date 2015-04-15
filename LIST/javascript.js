8c814391c38345e129a294b0a2668a88dc074d1a
abrahamperez <aperez2@kippnyccp.org>
2015-04-15 09:22:48 -0400

    Update javascript.js

--- LIST/javascript.js
+++ LIST/javascript.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-  $('#add').on('click', function(){
+  $('#add').on('click', function(){//add item
     var items=$('input').val();
     $("div").append('<p>'+items+'</p>');
   });
-  $("#remove").on('click', function(){
+  $("#remove").on('click', function(){//remove item
     var item=$('input').val();
     $('div').empty();
   });
