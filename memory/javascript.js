e906d88ce768ceb3f41076f0e0b1ce99f55f12c3
abrahamperez <aperez2@kippnyccp.org>
2015-04-15 09:17:21 -0400

    Update javascript.js

--- memory/javascript.js
+++ memory/javascript.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
   var first_guess=0;
   var second_guess=0;
   var first_card=0;//these are variables that will compare the numbers
-  var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"];
+  var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"];//numbers on cards
   
   function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex ;
+    var currentIndex = array.length, temporaryValue, randomIndex ;//change order of cards
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
     // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
+      randomIndex = Math.floor(Math.random() * currentIndex);//random order
       currentIndex -= 1;
     // And swap it with the current element.
       temporaryValue = array[currentIndex];
@@ -26,17 +26,17 @@ for(i in shuffle2){//this is the numbers that will appear on each card
   }
   
   $("div.card").click(function(){
-    if(click_count == 0){
+    if(click_count == 0){//limit the clicks and cards that face up
       $(this).find('p').css("opacity","1").addClass('potato');
       first_guess=$(this).find('p').html();
       click_count = 1;
     }
     else{
-      $(this).find('p').css("opacity","1").addClass('potato');
+      $(this).find('p').css("opacity","1").addClass('potato');//place cards down
       second_guess=$(this).find('p').html();
       click_count = 0;
       if(first_guess == second_guess){
-        $('.potato').css('color', 'blue').removeClass('potato');
+        $('.potato').css('color', 'blue').removeClass('potato');//when same color change
       }
       else{
         setTimeout(function(){$('.potato').css('opacity', 0).removeClass('potato')}, 1000);
