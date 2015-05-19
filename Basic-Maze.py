4a1d847c27b6b6a657162ba263359fba46afd169
abrahamperez <aperez2@kippnyccp.org>
2015-05-19 09:07:16 -0400

    Update Basic-Maze.py

--- Basic-Maze.py
+++ Basic-Maze.py
@@ -1,2 +1,22 @@
 from gopigo import *
-while true:
+import time
+import math
+set_speed(100)
+
+enc_tgt(1, 1, 18)
+time.sleep(.1)
+fwd()
+time.sleep(3)
+
+def move_forward(feet):
+  mm = feet * 304.8
+  steps = mm/11.34
+  enc_tgt(1, 1, steps)
+  time.sleep(.1)
+  fwd()
+  time.sleep(3 * feet)
+
+def right():
+  enc_tgt(1, 0, steps)
+  time.sleep(.1)
+  
