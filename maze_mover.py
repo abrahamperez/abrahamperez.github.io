7f67237d19b9910c548836ebc4edfca53e344bba
abrahamperez <aperez2@kippnyccp.org>
2015-06-03 09:04:53 -0400

    Update maze_mover.py

--- maze_mover.py
+++ maze_mover.py
@@ -1,7 +1,26 @@
 from gopigo import *
 import time
 enable_servo()
-while us_dist(15)> 200:
+def turnleft():
+  enc_tgt(0, 1, 14 steps)
+  time.sleep(.1)
+  left()
+  time.sleep(.1)
+def turnright():
+  enc_tgt(1, 0, 14 steps)
+  time.sleep(.1)
+  right()
+  time.sleep(.1)
+while us_dist(15) > 100:
   fwd()
-for i in range(180):
+servo(0)
+if us_dist(15) > 100:
+  turnleft()
+  fwd()
+ 
+else:
+  servo(180)
+  turnright()
+  fwd()
+  
   
